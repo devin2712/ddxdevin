5e0995bbec36d83648b07f61a9c77f6a39482631
Devin Nguyen <devin@cloudhealthtech.com>
2020-05-24 19:08:06 +0000

    Only use localStorage if available on client

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -9,20 +9,54 @@ export interface ThemeProviderProps {
   children: React.ReactNode;
 }
 
+// Disable for safari due to safari security limitations
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function isLocalStorageAvailable() {
+  var storage;
+  try {
+    storage = window["localStorage"];
+    var x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
 export function ThemeProvider({ children }: ThemeProviderProps) {
   const [dark, setDark] = useState(false);
+  const [storageAvailable, setStorageAvailable] = useState(false);
 
   useEffect(() => {
-    const lastTheme = window.localStorage.getItem("darkTheme");
+    setStorageAvailable(isLocalStorageAvailable());
 
-    if (lastTheme === "true") {
-      setDark(true);
-      applyTheme("dark");
-    } else {
-      setDark(false);
-      applyTheme("light");
+    if (storageAvailable) {
+      const lastTheme = window.localStorage.getItem("darkTheme");
+
+      if (lastTheme === "true") {
+        setDark(true);
+        applyTheme("dark");
+      } else {
+        setDark(false);
+        applyTheme("light");
+      }
     }
-  });
+  }, []);
 
   const applyTheme = (theme) => {
     const root = document.getElementsByTagName("html")[0];
@@ -31,7 +65,10 @@ export function ThemeProvider({ children }: ThemeProviderProps) {
 
   const toggle = () => {
     setDark(!dark);
-    window.localStorage.setItem("darkTheme", String(!dark));
+    applyTheme(dark ? "light" : "dark");
+    if (storageAvailable) {
+      window.localStorage.setItem("darkTheme", String(!dark));
+    }
   };
 
   return (
