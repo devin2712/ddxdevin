9641b1b966122c8fa63e9ed053077ff0c6b5e331
Devin Nguyen <devin2712@gmail.com>
2024-07-04 11:09:40 -0400

    Add build hook to keep CSV file updated

---
+++ updateConcertsCsv.mjs
@@ -0,0 +1,38 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const csvUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7cRqq437Wt4-eWBZkbUUmO1GnCUQ-V_f4e9-VVwPS0hbD5vQDgFWzvgm16hMvDSLOtgRF8TBgRsvM/pub?gid=0&single=true&output=csv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const localCsvPath = path.join(__dirname, "public/docs", "concerts-backup.csv");
+
+const fetchAndSaveCsv = async () => {
+  try {
+    const response = await fetch(csvUrl);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch CSV: ${response.statusText}`);
+    }
+
+    const csvData = await response.text();
+
+    // Write the new data to a temporary file first
+    const tempCsvPath = path.join(__dirname, "public", "temp_concerts_backup.csv");
+    fs.writeFileSync(tempCsvPath, csvData, "utf8");
+
+    // Once the write to the temp file is successful, rename it to the original file
+    fs.renameSync(tempCsvPath, localCsvPath);
+    console.log("✅ CSV file successfully fetched and overwritten.");
+  } catch (error) {
+    console.error(
+      "⚠️ Skipping CSV overwrite, error fetching or saving CSV file:",
+      error
+    );
+  }
+};
+
+fetchAndSaveCsv();
