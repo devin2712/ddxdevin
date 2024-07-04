ffab79bc75e0ad17bf1527c3c338aedd9d952fbc
Devin Nguyen <devin2712@gmail.com>
2024-07-04 10:49:53 -0400

    Add set list styling

--- pages/concerts.tsx
+++ pages/concerts.tsx
@@ -16,6 +16,7 @@ import React from "react";
 import { decodeQuery, encodeQuery } from "../utils/encoder";
 import { normalizeString } from "../utils/stringNormalizer";
 import useBrowserLocale from "../hooks/useBrowserLocale";
+import { ARTIST_KEY, ArtistDisplay } from "../components/ArtistDisplay";
 
 export default function Concerts() {
   const { formatMessage } = useIntl();
@@ -31,7 +32,6 @@ export default function Concerts() {
 
   const CONCERT_DATA_URL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7cRqq437Wt4-eWBZkbUUmO1GnCUQ-V_f4e9-VVwPS0hbD5vQDgFWzvgm16hMvDSLOtgRF8TBgRsvM/pub?gid=0&single=true&output=csv";
-  const ARTIST_KEY = "ARTIST NAME";
 
   useEffect(() => {
     if (q) {
@@ -84,7 +84,7 @@ export default function Concerts() {
     return parseCSV(data);
   }, [data]);
 
-  const lastTimestamp = useMemo((): (Date | null) => {
+  const lastTimestamp = useMemo((): Date | null => {
     if (!data) return null;
 
     const headers = getHeaders(data);
@@ -125,54 +125,97 @@ export default function Concerts() {
           content={formatMessage({ id: "ddxdevin.concerts.title" })}
         />
       </Head>
+      <div className={styles.ductTapeContainer}>
+        <div className={styles.ductTape}></div>
+      </div>
       <BlogPost title={formatMessage({ id: "ddxdevin.concerts.title" })}>
         <section className={styles.concertLog}>
-          <p>
-            <strong className={styles.showCount}>(#)</strong>{" "}
-            {formatMessage({ id: "ddxdevin.concerts.indicator" })}
-          </p>
           {!data && (
             <p>{formatMessage({ id: "ddxdevin.concerts.loading" })}...</p>
           )}
         </section>
+
         {data && (
-          <>
-            <input
-              autoFocus
-              type="text"
-              placeholder={`${formatMessage({
-                id: "ddxdevin.concerts.search",
-              })}...`}
-              value={searchQuery}
-              onChange={handleSearch}
-            />
-            <span className={styles.resultsCount}>
-              {resultsCount[0]} / {resultsCount[1]}
-            </span>
-            <ul className={styles.artistList}>
-              {filteredRows.map((artist, idx) => (
-                <li key={idx}>
-                  <Highlight
-                    text={artist[ARTIST_KEY]}
-                    highlight={debouncedSearchQuery}
-                  />
-                  {artist["SHOWS"] ? (
-                    <strong
-                      className={styles.showCount}
-                    >{`(${artist["SHOWS"]})`}</strong>
-                  ) : null}
-                </li>
-              ))}
-            </ul>
-            <section className={styles.lastUpdated}>
-              <span>
-                {formatMessage({ id: "ddxdevin.concerts.lastUpdated" })}:{" "}
-              </span>
-              <pre>
-                {lastTimestamp?.toLocaleDateString(locale)}
+          <main className={styles.concertLogList}>
+            {lastTimestamp && (
+              <div className={styles.lastUpdated}>
+                <div>
+                  <pre>
+                    {formatMessage({ id: "ddxdevin.concerts.lastUpdated" })}:{" "}
+                    {new Intl.DateTimeFormat(locale, {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    }).format(lastTimestamp)}
+                  </pre>
+                </div>
+              </div>
+            )}
+
+            <div className={styles.setListHairlineContainer}>
+              <div className={styles.setListHairline}></div>
+            </div>
+
+            <div className={styles.search}>
+              <input
+                autoFocus
+                type="text"
+                placeholder={`${formatMessage({
+                  id: "ddxdevin.concerts.search",
+                })}...`}
+                value={searchQuery}
+                onChange={handleSearch}
+              />
+              <pre className={styles.resultsCount}>
+                {resultsCount[0]} / {resultsCount[1]}
               </pre>
-            </section>
-          </>
+            </div>
+
+            <div className={styles.indicator}>
+              <pre className={styles.indicator}>
+                <strong className={styles.showCount}>(#)</strong>{" "}
+                {formatMessage({ id: "ddxdevin.concerts.indicator" })}
+              </pre>
+            </div>
+
+            {debouncedSearchQuery ? (
+              <>
+                <ul className={styles.artistList}>
+                  {filteredRows.map((artist, idx) => (
+                    <li key={idx}>
+                      <ArtistDisplay
+                        artist={artist}
+                        query={debouncedSearchQuery}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <>
+                <ul className={styles.artistList}>
+                  {filteredRows.slice(0, -1).map((artist, idx) => (
+                    <li key={idx}>
+                      <ArtistDisplay artist={artist} />
+                    </li>
+                  ))}
+                </ul>
+                <h3 className={styles.encore}>
+                  {formatMessage({ id: "ddxdevin.concerts.encore" })}
+                </h3>
+                <ul className={styles.artistList}>
+                  <li>
+                    <ArtistDisplay
+                      artist={filteredRows[filteredRows.length - 1]}
+                    />
+                  </li>
+                </ul>
+              </>
+            )}
+            <div className={styles.ductTapeContainer}>
+              <div className={styles.ductTape}></div>
+            </div>
+          </main>
         )}
       </BlogPost>
     </Layout>
